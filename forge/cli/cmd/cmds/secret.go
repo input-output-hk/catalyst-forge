516b6f408a59aaf83623c577d7a6e3d08f381fa8
Joshua Gilman <joshuagilman@gmail.com>
2024-08-29 14:53:16 -0400

    feat: adds support for reading/writing secrets directly from a blueprint file

--- forge/cli/cmd/cmds/secret.go
+++ forge/cli/cmd/cmds/secret.go
@@ -6,6 +6,8 @@ import (
 	"log/slog"
 	"strings"
 
+	"github.com/input-output-hk/catalyst-forge/blueprint/pkg/loader"
+	"github.com/input-output-hk/catalyst-forge/blueprint/schema"
 	"github.com/input-output-hk/catalyst-forge/forge/cli/pkg/secrets"
 )
 
@@ -15,15 +17,18 @@ const (
 )
 
 type Get struct {
-	Provider string `short:"p" help:"The provider of the secret store." default:"aws"`
-	Path     string `arg:"" help:"The path of the secret."`
-	Key      string `arg:"" help:"The key inside of the secret to get."`
+	Blueprint string `short:"b" help:"Path to a blueprint file (or directory)."`
+	Key       string `short:"k" help:"The key inside of the secret to get."`
+	Provider  string `short:"p" help:"The provider of the secret store." default:"aws"`
+	Path      string `arg:"" help:"The path to the secret (or path in a blueprint if --blueprint is specified)."`
 }
 
 type Set struct {
-	Field    []string `short:"f" help:"A secret field to set."`
-	Provider string   `short:"p" help:"The provider of the secret store." default:"aws"`
-	Path     string   `arg:"" help:"The path of the secret."`
+	Blueprint string   `short:"b" help:"Path to a blueprint file (or directory)."`
+	Field     []string `short:"f" help:"A secret field to set."`
+	Provider  string   `short:"p" help:"The provider of the secret store." default:"aws"`
+	Path      string   `arg:"" help:"The path to the secret (or path in a blueprint if --blueprint is specified)."`
+	Value     string   `arg:"" help:"The value to set." default:""`
 }
 
 type SecretCmd struct {
@@ -32,56 +37,113 @@ type SecretCmd struct {
 }
 
 func (c *Get) Run(logger *slog.Logger) error {
+	var path, provider string
+
+	if c.Blueprint != "" {
+		loader := loader.NewDefaultBlueprintLoader(c.Blueprint, logger)
+		if err := loader.Load(); err != nil {
+			return fmt.Errorf("could not load blueprint: %w", err)
+		}
+
+		rbp := loader.Raw()
+
+		var secret schema.Secret
+		if err := rbp.DecodePath(c.Path, &secret); err != nil {
+			return fmt.Errorf("could not decode secret: %w", err)
+		}
+
+		if secret.Path != "" && secret.Provider != "" {
+			path = secret.Path
+			provider = secret.Provider
+		}
+	} else {
+		path = c.Path
+		provider = c.Provider
+	}
+
 	store := secrets.NewDefaultSecretStore()
-	client, err := store.NewClient(logger, secrets.Provider(c.Provider))
+	client, err := store.NewClient(logger, secrets.Provider(provider))
 	if err != nil {
 		logger.Error("Unable to create secret client.", "err", err)
 		return fmt.Errorf("unable to create secret client: %w", err)
 	}
 
-	s, err := client.Get(c.Path)
+	s, err := client.Get(path)
 	if err != nil {
 		return fmt.Errorf("could not get secret: %w", err)
 	}
 
-	m := make(map[string]string)
+	if c.Key != "" {
+		m := make(map[string]string)
 
-	if err := json.Unmarshal([]byte(s), &m); err != nil {
-		return err
+		if err := json.Unmarshal([]byte(s), &m); err != nil {
+			return err
+		}
+
+		if _, ok := m[c.Key]; !ok {
+			return fmt.Errorf("key %s not found in secret at %s", c.Key, path)
+		}
+
+		fmt.Println(m[c.Key])
+	} else {
+		fmt.Println(s)
 	}
-
-	if _, ok := m[c.Key]; !ok {
-		return fmt.Errorf("key %s not found in secret at %s", c.Key, c.Path)
-	}
-
-	fmt.Println(m[c.Key])
 	return nil
 }
 
 func (c *Set) Run(logger *slog.Logger) error {
+	var path, provider string
+
+	if c.Blueprint != "" {
+		loader := loader.NewDefaultBlueprintLoader(c.Blueprint, logger)
+		if err := loader.Load(); err != nil {
+			return fmt.Errorf("could not load blueprint: %w", err)
+		}
+
+		rbp := loader.Raw()
+
+		var secret schema.Secret
+		if err := rbp.DecodePath(c.Path, &secret); err != nil {
+			return fmt.Errorf("could not decode secret: %w", err)
+		}
+
+		if secret.Path != "" && secret.Provider != "" {
+			path = secret.Path
+			provider = secret.Provider
+		}
+	} else {
+		path = c.Path
+		provider = c.Provider
+	}
+
 	store := secrets.NewDefaultSecretStore()
-	client, err := store.NewClient(logger, secrets.Provider(secrets.ProviderAWS))
+	client, err := store.NewClient(logger, secrets.Provider(provider))
 	if err != nil {
 		logger.Error("Unable to create secret client.", "err", err)
 		return fmt.Errorf("unable to create secret client: %w", err)
 	}
 
-	fields := make(map[string]string)
-	for _, f := range c.Field {
-		kv := strings.Split(f, "=")
-		if len(kv) != 2 {
-			return fmt.Errorf("invalid field format: %s: must be in the format of key=value", f)
+	var data []byte
+	if len(c.Field) > 0 {
+		fields := make(map[string]string)
+		for _, f := range c.Field {
+			kv := strings.Split(f, "=")
+			if len(kv) != 2 {
+				return fmt.Errorf("invalid field format: %s: must be in the format of key=value", f)
+			}
+
+			fields[kv[0]] = kv[1]
 		}
 
-		fields[kv[0]] = kv[1]
+		data, err = json.Marshal(&fields)
+		if err != nil {
+			return err
+		}
+	} else {
+		data = []byte(c.Value)
 	}
 
-	b, err := json.Marshal(&fields)
-	if err != nil {
-		return err
-	}
-
-	id, err := client.Set(c.Path, string(b))
+	id, err := client.Set(path, string(data))
 	if err != nil {
 		logger.Error("could not set secret", "err", err)
 		return err
