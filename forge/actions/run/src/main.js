03d0d2e0ba457be8460d98c339173f96d5de09b5
Joshua Gilman <joshuagilman@gmail.com>
2024-08-30 19:54:18 -0400

    wip: testing

---
+++ forge/actions/run/src/main.js
@@ -0,0 +1,42 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+async function run() {
+  try {
+    const artifact = core.getBooleanInput("artifact", { required: false });
+    const local = core.getBooleanInput("local", { required: false });
+    const path = core.getInput("path", { required: true });
+
+    let args = ["-vv", "run"];
+
+    if (artifact === true) {
+      args.push("--artifact");
+    }
+
+    if (local === true) {
+      args.push("--local");
+    }
+
+    args.push(path);
+
+    core.info(`Running forge ${args.join(" ")}`);
+
+    let stdout = "";
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        stdout += data.toString();
+      },
+    };
+
+    await exec.exec("forge", args, options);
+
+    core.setOutput("result", stdout);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = {
+  run,
+};
