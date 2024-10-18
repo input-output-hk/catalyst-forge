18c608051fa53eb5992cf2be2627f21512d9e753
Joshua Gilman <joshuagilman@gmail.com>
2024-10-17 17:29:10 -0700

    wip: cleanup

--- lib/tools/git/branch.go
+++ lib/tools/git/branch.go
@@ -14,20 +14,14 @@ var (
 
 func GetBranch(repo *gg.Repository) (string, error) {
 	if InCI() {
-		fmt.Println("Running in CI")
 		ref, ok := os.LookupEnv("GITHUB_HEAD_REF")
 		if !ok || ref == "" {
 			if strings.HasPrefix(os.Getenv("GITHUB_REF"), "refs/heads/") {
-				fmt.Printf("Branch from GITHUB_REF: %s\n", os.Getenv("GITHUB_REF"))
-				fmt.Println(strings.TrimPrefix(os.Getenv("GITHUB_REF"), "refs/heads/"))
 				return strings.TrimPrefix(os.Getenv("GITHUB_REF"), "refs/heads/"), nil
 			}
 
-			fmt.Println("Found no CI branch")
-
 			// Revert to trying to get the branch from the local repository
 		} else {
-			fmt.Printf("Branch from GITHUB_HEAD_REF: %s\n", ref)
 			return ref, nil
 		}
 	}
