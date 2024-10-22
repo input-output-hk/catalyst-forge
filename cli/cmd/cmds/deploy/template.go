f573d8107aaa095804901467b267096e75079e23
Joshua Gilman <joshuagilman@gmail.com>
2024-10-22 13:09:34 -0700

    wip: finish implementation

--- cli/cmd/cmds/deploy/template.go
+++ cli/cmd/cmds/deploy/template.go
@@ -3,6 +3,7 @@ package deploy
 import (
 	"fmt"
 
+	"github.com/input-output-hk/catalyst-forge/cli/pkg/deployment"
 	"github.com/input-output-hk/catalyst-forge/cli/pkg/run"
 )
 
@@ -11,10 +12,27 @@ type TemplateCmd struct {
 }
 
 func (c *TemplateCmd) Run(ctx run.RunContext) error {
-	_, err := ctx.ProjectLoader.Load(c.Project)
+	project, err := ctx.ProjectLoader.Load(c.Project)
 	if err != nil {
 		return fmt.Errorf("could not load project: %w", err)
 	}
+
+	bundle, err := deployment.GenerateBundle(&project)
+	if err != nil {
+		return fmt.Errorf("could not generate bundle: %w", err)
+	}
+
+	templater, err := deployment.NewDefaultBundleTemplater(ctx.Logger)
+	if err != nil {
+		return fmt.Errorf("could not create bundle templater: %w", err)
+	}
+
+	out, err := templater.Render(bundle)
+	if err != nil {
+		return fmt.Errorf("could not render bundle: %w", err)
+	}
+
+	fmt.Println(out)
 
 	return nil
 }
