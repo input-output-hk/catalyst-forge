b8c6a2a24886fab7e18db43470d17a4f5b17acb9
Joshua Gilman <joshuagilman@gmail.com>
2025-01-27 16:09:00 -0800

    refactor: implements new GitRepo type for common operations

---
+++ lib/tools/git/repo/repo.go
@@ -0,0 +1,267 @@
+package repo
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"time"
+
+	gg "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+	"github.com/input-output-hk/catalyst-forge/lib/tools/git/repo/remote"
+	"github.com/spf13/afero"
+	df "gopkg.in/jfontan/go-billy-desfacer.v0"
+)
+
+const (
+	DEFAULT_AUTHOR = "Catalyst Forge"
+	DEFAULT_EMAIL  = "forge@projectcatalyst.io"
+)
+
+type GitRepoOption func(*GitRepo)
+
+// GitRepo is a high-level representation of a git repository.
+type GitRepo struct {
+	auth         *http.BasicAuth
+	basePath     string
+	commitAuthor string
+	commitEmail  string
+	fs           afero.Fs
+	logger       *slog.Logger
+	raw          *gg.Repository
+	remote       remote.GitRemoteInteractor
+	worktree     *gg.Worktree
+}
+
+// AddFile adds a file to the staging area.
+func (g *GitRepo) AddFile(path string) error {
+	_, err := g.worktree.Add(path)
+	if err != nil {
+		return fmt.Errorf("failed to add file: %w", err)
+	}
+
+	return nil
+}
+
+// Commit creates a commit with the given message.
+func (g *GitRepo) Commit(msg string) (plumbing.Hash, error) {
+	author, email := g.getAuthor()
+	hash, err := g.worktree.Commit(msg, &gg.CommitOptions{
+		Author: &object.Signature{
+			Name:  author,
+			Email: email,
+			When:  time.Now(),
+		},
+	})
+
+	if err != nil {
+		return plumbing.ZeroHash, err
+	}
+
+	return hash, nil
+}
+
+// Exists checks if a file exists in the repository.
+func (g *GitRepo) Exists(path string) (bool, error) {
+	_, err := g.fs.Stat(filepath.Join(g.basePath, path))
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	} else if err != nil {
+		return false, fmt.Errorf("failed to check if file exists: %w", err)
+	}
+
+	return true, nil
+}
+
+// GetCommit returns the commit with the given hash.
+func (g *GitRepo) GetCommit(hash plumbing.Hash) (*object.Commit, error) {
+	return g.raw.CommitObject(hash)
+}
+
+// HasChanges returns true if the repository has changes.
+func (g *GitRepo) HasChanges() (bool, error) {
+	status, err := g.worktree.Status()
+	if err != nil {
+		return false, err
+	}
+
+	return !status.IsClean(), nil
+}
+
+// Head returns the HEAD reference of the current branch.
+func (g *GitRepo) Head() (*plumbing.Reference, error) {
+	return g.raw.Head()
+}
+
+// LoadFromPath loads a repository from a local path.
+func (g *GitRepo) LoadFromPath(path string) error {
+	workdir := afero.NewBasePathFs(g.fs, path)
+	gitdir := afero.NewBasePathFs(g.fs, filepath.Join(path, ".git"))
+
+	storage := filesystem.NewStorage(df.New(gitdir), cache.NewObjectLRUDefault())
+	repo, err := gg.Open(storage, df.New(workdir))
+	if err != nil {
+		return fmt.Errorf("failed to open repository: %w", err)
+	}
+
+	wt, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("failed to get worktree: %w", err)
+	}
+
+	g.basePath = path
+	g.raw = repo
+	g.worktree = wt
+
+	return nil
+}
+
+// LoadFromRemote loads a repository from a git remote.
+func (g *GitRepo) LoadFromRemote(path, url, branch string) error {
+	workdir := afero.NewBasePathFs(g.fs, path)
+	gitdir := afero.NewBasePathFs(g.fs, filepath.Join(path, ".git"))
+	ref := fmt.Sprintf("refs/heads/%s", branch)
+
+	g.logger.Debug("Cloning repository", "url", url, "ref", ref)
+	storage := filesystem.NewStorage(df.New(gitdir), cache.NewObjectLRUDefault())
+	repo, err := g.remote.Clone(storage, df.New(workdir), &gg.CloneOptions{
+		URL:           url,
+		Depth:         1,
+		ReferenceName: plumbing.ReferenceName(ref),
+		Auth:          g.auth,
+	})
+
+	if err != nil {
+		return err
+	}
+
+	wt, err := repo.Worktree()
+	if err != nil {
+		return fmt.Errorf("failed to get worktree: %w", err)
+	}
+
+	g.basePath = path
+	g.raw = repo
+	g.worktree = wt
+
+	return nil
+}
+
+// MkdirAll creates a directory and all necessary parents.
+func (g *GitRepo) MkdirAll(path string) error {
+	return g.fs.MkdirAll(path, 0755)
+}
+
+// NewBranch creates a new branch with the given name.
+func (g *GitRepo) NewBranch(name string) error {
+	head, err := g.raw.Head()
+	if err != nil {
+		return fmt.Errorf("failed to get HEAD reference: %w", err)
+	}
+
+	return g.worktree.Checkout(&gg.CheckoutOptions{
+		Branch: plumbing.NewBranchReferenceName(name),
+		Hash:   head.Hash(),
+		Create: true,
+	})
+}
+
+// NewTag creates a new tag with the given name and message.
+func (g *GitRepo) NewTag(commit plumbing.Hash, name, message string) (*plumbing.Reference, error) {
+	author, email := g.getAuthor()
+	tag, err := g.raw.CreateTag(name, commit, &gg.CreateTagOptions{
+		Tagger: &object.Signature{
+			Name:  author,
+			Email: email,
+			When:  time.Now(),
+		},
+		Message: message,
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tag: %w", err)
+	}
+
+	return tag, nil
+}
+
+// ReadFile reads the contents of a file in the repository.
+func (g *GitRepo) ReadFile(path string) ([]byte, error) {
+	return afero.ReadFile(g.fs, path)
+}
+
+// Push pushes changes to the remote repository.
+func (g *GitRepo) Push() error {
+	return g.remote.Push(g.raw, &gg.PushOptions{
+		Auth: g.auth,
+	})
+}
+
+// Raw returns the underlying go-git repository.
+func (g *GitRepo) Raw() *gg.Repository {
+	return g.raw
+}
+
+// WriteFile writes the given contents to the given path in the repository.
+// It also automatically adds the file to the staging area.
+func (g *GitRepo) WriteFile(path string, contents []byte) error {
+	newPath := filepath.Join(g.basePath, path)
+	if err := afero.WriteFile(g.fs, newPath, contents, 0644); err != nil {
+		return fmt.Errorf("failed to write file: %w", err)
+	}
+
+	_, err := g.worktree.Add(path)
+	if err != nil {
+		return fmt.Errorf("failed to add file: %w", err)
+	}
+
+	return nil
+}
+
+// getAuthor returns the author and email for commits.
+func (g *GitRepo) getAuthor() (string, string) {
+	author := g.commitAuthor
+	email := g.commitEmail
+	if author == "" {
+		author = DEFAULT_AUTHOR
+	}
+
+	if email == "" {
+		email = DEFAULT_EMAIL
+	}
+
+	return author, email
+}
+
+// WithAuth sets the authentication for the interacting with a remote repository.
+func WithAuth(username, password string) GitRepoOption {
+	return func(g *GitRepo) {
+		g.auth = &http.BasicAuth{
+			Username: username,
+			Password: password,
+		}
+	}
+}
+
+// WithAuthor sets the author for all commits.
+func WithAuthor(name, email string) GitRepoOption {
+	return func(g *GitRepo) {
+		g.commitAuthor = name
+		g.commitEmail = email
+	}
+}
+
+// NewGitRepo creates a new GitRepo instance.
+func NewGitRepo(fs afero.Fs, logger *slog.Logger) GitRepo {
+	return GitRepo{
+		fs:     fs,
+		logger: logger,
+		remote: remote.GoGitRemoteInteractor{},
+	}
+}
---
+++ lib/tools/git/repo/repo_test.go
@@ -0,0 +1,296 @@
+package repo
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/go-git/go-billy/v5"
+	gg "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/cache"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage"
+	"github.com/go-git/go-git/v5/storage/filesystem"
+	"github.com/input-output-hk/catalyst-forge/lib/tools/git/repo/remote/mocks"
+	"github.com/input-output-hk/catalyst-forge/lib/tools/testutils"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	df "gopkg.in/jfontan/go-billy-desfacer.v0"
+)
+
+const (
+	repoPath = "/repo"
+)
+
+func TestGitRepoAddFile(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		repo := newGitRepo(t)
+		afero.WriteFile(repo.fs, joinPath("file.txt"), []byte("test"), 0644)
+
+		_, err := repo.worktree.Add("file.txt")
+		require.NoError(t, err, "failed to add file")
+
+		status, err := repo.worktree.Status()
+		require.NoError(t, err)
+
+		assert.Contains(t, status, "file.txt")
+	})
+
+	t.Run("file missing", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		err := repo.AddFile("file.txt")
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to add file")
+	})
+}
+
+func TestGitRepoCommit(t *testing.T) {
+	t.Run("succcess", func(t *testing.T) {
+		repo := newGitRepo(t)
+		err := afero.WriteFile(repo.fs, joinPath("file.txt"), []byte("test"), 0644)
+		require.NoError(t, err)
+
+		err = repo.AddFile("file.txt")
+		require.NoError(t, err)
+
+		hash, err := repo.Commit("test")
+		require.NoError(t, err)
+
+		commit, err := repo.GetCommit(hash)
+		require.NoError(t, err)
+		assert.Equal(t, commit.Message, "test")
+	})
+}
+
+func TestGitRepoExists(t *testing.T) {
+	t.Run("exists", func(t *testing.T) {
+		repo := newGitRepo(t)
+		err := afero.WriteFile(repo.fs, joinPath("file.txt"), []byte("test"), 0644)
+		require.NoError(t, err)
+
+		exists, err := repo.Exists("file.txt")
+		require.NoError(t, err)
+		assert.True(t, exists)
+	})
+
+	t.Run("does not exist", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		exists, err := repo.Exists("file.txt")
+		require.NoError(t, err)
+		assert.False(t, exists)
+	})
+}
+
+func TestGitRepoHasChanges(t *testing.T) {
+	t.Run("has changes", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		err := afero.WriteFile(repo.fs, joinPath("file.txt"), []byte("test"), 0644)
+		require.NoError(t, err)
+
+		changes, err := repo.HasChanges()
+		require.NoError(t, err)
+		assert.True(t, changes)
+	})
+
+	t.Run("no changes", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		changes, err := repo.HasChanges()
+		require.NoError(t, err)
+		assert.False(t, changes)
+	})
+}
+
+func TestGitRepoLoadFromPath(t *testing.T) {
+	repo := newRepo(t)
+
+	r := GitRepo{
+		fs:     repo.fs,
+		logger: testutils.NewNoopLogger(),
+	}
+
+	err := r.LoadFromPath(repoPath)
+	require.NoError(t, err)
+
+	head, err := r.raw.Head()
+	require.NoError(t, err)
+
+	commit, err := r.GetCommit(head.Hash())
+	require.NoError(t, err)
+	assert.Equal(t, commit.Message, "test")
+}
+
+func TestGitRepoLoadFromRemote(t *testing.T) {
+	repo := newRepo(t)
+
+	var opts *gg.CloneOptions
+	r := GitRepo{
+		fs:     repo.fs,
+		logger: testutils.NewNoopLogger(),
+		remote: &mocks.GitRemoteInteractorMock{
+			CloneFunc: func(s storage.Storer, worktree billy.Filesystem, o *gg.CloneOptions) (*gg.Repository, error) {
+				opts = o
+				return repo.repo, nil
+			},
+		},
+	}
+
+	err := r.LoadFromRemote(repoPath, "test.com", "master")
+	require.NoError(t, err)
+
+	assert.Equal(t, opts.URL, "test.com")
+	assert.Equal(t, opts.ReferenceName, plumbing.ReferenceName("refs/heads/master"))
+
+	head, err := r.raw.Head()
+	require.NoError(t, err)
+
+	commit, err := r.GetCommit(head.Hash())
+	require.NoError(t, err)
+	assert.Equal(t, commit.Message, "test")
+}
+
+func TestGitRepoNewBranch(t *testing.T) {
+	repo := newGitRepo(t)
+
+	err := repo.NewBranch("test")
+	require.NoError(t, err)
+
+	head, err := repo.raw.Head()
+	require.NoError(t, err)
+	assert.Equal(t, head.Name().String(), "refs/heads/test")
+}
+
+func TestGitRepoNewTag(t *testing.T) {
+	repo := newGitRepo(t)
+
+	err := afero.WriteFile(repo.fs, joinPath("file.txt"), []byte("test"), 0644)
+	require.NoError(t, err)
+
+	err = repo.AddFile("file.txt")
+	require.NoError(t, err)
+
+	hash, err := repo.Commit("test")
+	require.NoError(t, err)
+
+	tag, err := repo.NewTag(hash, "test", "test")
+	require.NoError(t, err)
+
+	assert.Equal(t, tag.Name().String(), "refs/tags/test")
+}
+
+func TestGitRepoWriteFile(t *testing.T) {
+	repo := newGitRepo(t)
+
+	err := repo.WriteFile("file.txt", []byte("test"))
+	require.NoError(t, err)
+
+	status, err := repo.worktree.Status()
+	require.NoError(t, err)
+	assert.Contains(t, status, "file.txt")
+}
+
+func TestGitRepoPush(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		var opts *gg.PushOptions
+		auth := &http.BasicAuth{
+			Username: "forge",
+			Password: "test",
+		}
+
+		repo.auth = auth
+		repo.remote = &mocks.GitRemoteInteractorMock{
+			PushFunc: func(r *gg.Repository, o *gg.PushOptions) error {
+				opts = o
+				return nil
+			},
+		}
+
+		err := repo.Push()
+		assert.NoError(t, err)
+		assert.Equal(t, opts.Auth, auth)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		repo := newGitRepo(t)
+
+		repo.remote = &mocks.GitRemoteInteractorMock{
+			PushFunc: func(r *gg.Repository, o *gg.PushOptions) error {
+				return fmt.Errorf("failed to push")
+			},
+		}
+
+		err := repo.Push()
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "failed to push")
+	})
+}
+
+func joinPath(path string) string {
+	return filepath.Join(repoPath, path)
+}
+
+type testRepo struct {
+	repo     *gg.Repository
+	fs       afero.Fs
+	worktree *gg.Worktree
+}
+
+func newRepo(t *testing.T) testRepo {
+	fs := afero.NewMemMapFs()
+	rfs := afero.NewBasePathFs(fs, repoPath)
+	wfs := df.New(rfs)
+	gitdir := afero.NewBasePathFs(rfs, ".git")
+	storage := filesystem.NewStorage(df.New(gitdir), cache.NewObjectLRUDefault())
+
+	repo, err := gg.Init(storage, wfs)
+	require.NoError(t, err, "failed to init repo")
+
+	worktree, err := repo.Worktree()
+	require.NoError(t, err, "failed to get worktree")
+
+	afero.WriteFile(fs, filepath.Join(repoPath, "test.txt"), []byte("test"), 0644)
+
+	_, err = worktree.Add("test.txt")
+	require.NoError(t, err, "failed to add file")
+
+	status, err := worktree.Status()
+	require.NoError(t, err)
+	assert.False(t, status.IsClean())
+	assert.Contains(t, status, "test.txt")
+
+	_, err = worktree.Commit("test", &gg.CommitOptions{
+		Author: &object.Signature{
+			Name:  "test",
+			Email: "test@test.com",
+			When:  time.Now(),
+		},
+	})
+	require.NoError(t, err, "failed to commit")
+
+	return testRepo{
+		fs:       fs,
+		repo:     repo,
+		worktree: worktree,
+	}
+}
+
+func newGitRepo(t *testing.T) GitRepo {
+	repo := newRepo(t)
+
+	return GitRepo{
+		basePath: "/repo",
+		fs:       repo.fs,
+		logger:   testutils.NewNoopLogger(),
+		raw:      repo.repo,
+		worktree: repo.worktree,
+	}
+}
