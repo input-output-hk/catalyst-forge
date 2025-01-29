cbc49ece17163a4de99277b82d48cde6870dc09a
Joshua Gilman <joshuagilman@gmail.com>
2025-01-28 16:22:53 -0800

    wip: fix

--- lib/project/blueprint/defaults/deployment.go
+++ lib/project/blueprint/defaults/deployment.go
@@ -10,11 +10,7 @@ import (
 type DeploymentModuleSetter struct{}
 
 func (d DeploymentModuleSetter) SetDefault(v cue.Value) (cue.Value, error) {
-	projectName, err := v.LookupPath(cue.ParsePath("project.name")).String()
-	if err != nil {
-		return v, fmt.Errorf("failed to get project name: %w", err)
-	}
-
+	projectName, _ := v.LookupPath(cue.ParsePath("project.name")).String()
 	registry, _ := v.LookupPath(cue.ParsePath("global.deployment.registries.modules")).String()
 
 	modules := v.LookupPath(cue.ParsePath("project.deployment.modules"))
@@ -31,9 +27,11 @@ func (d DeploymentModuleSetter) SetDefault(v cue.Value) (cue.Value, error) {
 		moduleName := iter.Selector().String()
 		module := iter.Value()
 
-		instance := module.LookupPath(cue.ParsePath("instance"))
-		if !instance.Exists() {
-			v = v.FillPath(cue.ParsePath(fmt.Sprintf("project.deployment.modules.%s.instance", moduleName)), projectName)
+		if projectName != "" {
+			instance := module.LookupPath(cue.ParsePath("instance"))
+			if !instance.Exists() {
+				v = v.FillPath(cue.ParsePath(fmt.Sprintf("project.deployment.modules.%s.instance", moduleName)), projectName)
+			}
 		}
 
 		if registry != "" {
