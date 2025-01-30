5ed5c04a19184be3f368bf9dcd369b2fc4f0fd04
Joshua Gilman <joshuagilman@gmail.com>
2025-01-29 18:41:07 -0800

    feat(cli): adds dump command and allows template command to process modules (#129)

--- cli/cmd/cmds/module/template.go
+++ cli/cmd/cmds/module/template.go
@@ -2,24 +2,45 @@ package module
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/input-output-hk/catalyst-forge/cli/pkg/run"
+	"github.com/input-output-hk/catalyst-forge/lib/project/deployment"
+	"github.com/input-output-hk/catalyst-forge/lib/project/schema"
 )
 
 type TemplateCmd struct {
-	Module string `arg:"" help:"The path to the module (or project)." kong:"arg,predictor=path"`
+	Path string `arg:"" help:"The path to the module (or project)." kong:"arg,predictor=path"`
 }
 
 func (c *TemplateCmd) Run(ctx run.RunContext) error {
-	project, err := ctx.ProjectLoader.Load(c.Module)
+	stat, err := os.Stat(c.Path)
 	if err != nil {
-		return fmt.Errorf("could not load project: %w", err)
+		return fmt.Errorf("could not stat path: %w", err)
 	}
 
-	modules := project.Blueprint.Project.Deployment.Modules
+	var bundle schema.DeploymentModuleBundle
+	if stat.IsDir() {
+		project, err := ctx.ProjectLoader.Load(c.Path)
+		if err != nil {
+			return fmt.Errorf("could not load project: %w", err)
+		}
 
-	result, err := ctx.DeploymentGenerator.GenerateBundle(modules)
+		bundle = project.Blueprint.Project.Deployment.Modules
+	} else {
+		src, err := os.ReadFile(c.Path)
+		if err != nil {
+			return fmt.Errorf("could not read file: %w", err)
+		}
+
+		bundle, err = deployment.ParseBundle(src)
+		if err != nil {
+			return fmt.Errorf("could not parse module file: %w", err)
+		}
+	}
+
+	result, err := ctx.DeploymentGenerator.GenerateBundle(bundle)
 	if err != nil {
 		return fmt.Errorf("failed to generate manifests: %w", err)
 	}
